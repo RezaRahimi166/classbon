f9dbbfea479404fbe4e3b9402a891a6ad8b5ac6c
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-19 13:35:55 -0900

    feat(routing): creating routes

---
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogDetails = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+  return (
+    <div className="text-5xl flex justify-center items-center">
+      <h1>{slug}</h1>
+    </div>
+  );
+};
+
+export default BlogDetails;
---
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function CoursDetails() {
+  return (
+    <div className="text-5xl flex justify-center items-center">
+      <h1>this is CoursDetails page</h1>
+    </div>
+  );
+}
