93a910288a8f415b32e9e96eec4f2c7a943a8cc2
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-20 11:00:48 -0900

    feat(header): creating header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Figtree } from "next/font/google";
 import localFont from "next/font/local";
+import { Header } from "./_components/header";
+import { Footer } from "./_components/footer";
 
 const figtree = Figtree({
   display: "swap",
@@ -41,17 +43,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html dir="rtl" className={`${figtree.variable}  ${vazir.variable}`}>
-      <body className="flex flex-col min-h-screen font-bold uppercase">
-        <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          دوره ی معماری ریکت
-        </header>
+    <html dir="rtl" className={` dark ${figtree.variable}  ${vazir.variable}`}>
+      <body className=" min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+        <Header />
         <div className="flex-1 flex  justify-center items-center">
           {children}
         </div>
-        <footer className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          FOOTER
-        </footer>
+        <Footer />
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,8 +51,11 @@ const config: Config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     colors: tailwindColors,
-    extend: {},
+    container: {
+      center: true,
+    },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
