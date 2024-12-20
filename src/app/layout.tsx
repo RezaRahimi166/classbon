f2f72d9148515d9b5bd109a1e1d0d1597136bc83
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-19 15:19:52 -0900

    feat(font): applying fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,39 @@
 import "./globals.css";
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const vazir = localFont({
+  src: [
+    {
+      path: "../../public/fonts/vazir/Vazirmatn-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazir/Vazirmatn-Medium.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazir/Vazirmatn-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazir/Vazirmatn-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-vazir",
+});
 
 export default function RootLayout({
   children,
@@ -6,10 +41,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl">
+    <html dir="rtl" className={`${figtree.variable}  ${vazir.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          HEADER
+          دوره ی معماری ریکت
         </header>
         <div className="flex-1 flex  justify-center items-center">
           {children}
