b5bc45933ccf670a93791dd255a059a1a8a41cc8
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-21 19:24:49 -0900

    feat(testing): installing test-library jest and config & apply some test for button

---
+++ src/app/_components/button/button.test.tsx
@@ -0,0 +1,23 @@
+import { describe } from "node:test";
+import { render, screen } from "@testing-library/react";
+import { Button } from "./button";
+
+describe("Button Components", () => {
+  test("renders a default button", () => {
+    const { getByText } = render(<Button>Click here</Button>);
+    expect(getByText("Click here")).toBeInTheDocument();
+  });
+
+  test("disables the button when isDisabled prop is true", () => {
+    render(<Button isDisabled={true}>Click here</Button>);
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  test("applies the correct css class for diffrent button variants ", () => {
+    const { rerender } = render(<Button variant="primary">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-primary");
+
+    rerender(<Button variant="info">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-info");
+  });
+});
