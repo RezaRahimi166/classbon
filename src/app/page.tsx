5b89654adb194b66ca87edb3df2173e99dea1387
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-23 17:24:56 -0900

    feat(feature): creating feature component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { homeFeatures } from "@/data/home-features";
 import { CourseCardList } from "./(courses)/_components/course-card-list";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
 import { CourseSummary } from "@/types/course-summary.interface";
+import Feature from "./_components/feature/feature";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -20,6 +22,14 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
+      <section className="dark:bg-base-75 mt-10">
+        <div className="container py-10 flex flex-col lg:flex-row gap-10 xl:gap-5">
+          {homeFeatures.map((feature) => (
+            <Feature key={`feature-${feature.title}`} feature={feature} />
+          ))}
+        </div>
+      </section>
+
       <section className="container pt-20">
         <div className="text-center xl:text-right">
           <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
