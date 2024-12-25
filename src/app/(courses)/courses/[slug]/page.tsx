968a61b9a26b34c6143d48d1dcf138b05750887d
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-25 13:42:24 -0900

    feat(course-aside): creating course-aside page for details

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { Progress } from "@/app/_components/progress";
 import { Rating } from "@/app/_components/rating/rating";
 import { API_URL } from "@/configs/global";
 import { CourseDetails } from "@/types/course-details.interface";
+import { CourseAside } from "./_components/course-aside/course-aside";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
@@ -26,7 +27,7 @@ export default async function CoursDetails({
   const { slug } = params;
   const course = await getCourse(slug);
   return (
-    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+    <div className="p-5 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
       <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
       <div className="col-span-10 xl:col-span-7">
         <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
@@ -39,8 +40,7 @@ export default async function CoursDetails({
         <div className=" mt-5">Video Player Component</div>
       </div>
       <div className="col-span-10 xl:col-span-3">
-        <Rating rate={3} />
-        <Progress value={55} variant="primary" size="tiny" />
+        <CourseAside {...course} />
       </div>
       <div className="col-span-10 xl:col-span-6 bg-info"></div>
       <div className="col-span-10 xl:col-span-4 bg-warning"></div>
