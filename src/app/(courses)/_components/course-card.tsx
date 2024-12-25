6ed041e0dbe49fb5cf35963e2e700dc876875084
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-25 12:20:56 -0900

    feat(course-detail): working on corese-detail page

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -43,7 +43,7 @@ export const CourseCard: React.FC<CourseTypeProps> = ({
         </div>
       </div>
       <Link
-        href={`/course/${slug}`}
+        href={`/courses/${slug}`}
         className="card-footer justify-center animated-icon"
       >
         مشاهده ی جزئیات دوره
--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,4 +1,28 @@
-export default async function CoursDetails() {
+import { API_URL } from "@/configs/global";
+import { CourseDetails } from "@/types/course-details.interface";
+
+export async function generateStaticParams() {
+  const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
+    res.json()
+  );
+
+  return (slugs as string[]).map((slug) => ({
+    slug: slug,
+  }));
+}
+
+async function getCourse(slug: string): Promise<CourseDetails> {
+  const res = await fetch(`${API_URL}/courses/${slug}`);
+  return res.json();
+}
+
+export default async function CoursDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+  const courseData = await getCourse(slug);
   return (
     <div className="text-5xl flex justify-center items-center">
       <h1>this is CoursDetails page</h1>
