a8922f34d067dd79ac99aca9265892205b87c291
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-12-29 10:25:16 -0900

    feat(infinite scroll): comments scroll

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -3,20 +3,54 @@
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/_components/comment/comment";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { TextPlaceholder } from "@/app/_components/placeholders";
 
 const CourseComments = () => {
+  const { ref, inView } = useInView({});
+
   const { slug } = useParams();
-  const { data: comments, isLoading } = useCourseComments({
+  const {
+    data: comments,
+    error,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
     },
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
   return (
     <>
-      {comments?.data.map((comment) => (
-        <Comment key={`comment-${comment.id}`} {...comment} variant={"info"} />
+      {comments?.pages.map((currentPage) => (
+        <Fragment key={`comment-page-${currentPage}`}>
+          {currentPage.data.map((comment) => (
+            <Comment
+              key={`comment-${comment.id}`}
+              {...comment}
+              variant={"info"}
+            />
+          ))}
+        </Fragment>
       ))}
+
+      {isFetchingNextPage ||
+        (hasNextPage && (
+          <div ref={ref}>
+            <TextPlaceholder />
+          </div>
+        ))}
     </>
   );
 };
