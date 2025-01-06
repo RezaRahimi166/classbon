2eb2c0b365a41075539847d1830f087d940d774b
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-01-05 19:29:51 -0900

    feat(tol-loader): adding tp loader to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import { Header } from "./_components/header";
 import { Footer } from "./_components/footer";
 import QueryProvider from "@/providers/react-query-provider";
+import NextTopLoader from "nextjs-toploader";
 
 const figtree = Figtree({
   display: "swap",
@@ -46,6 +47,7 @@ export default function RootLayout({
   return (
     <html dir="rtl" className={` dark ${figtree.variable}  ${vazir.variable}`}>
       <body className=" min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+        <NextTopLoader showSpinner={false} color="var(--color-primary)" />
         <QueryProvider>
           <Header />
           <main>{children}</main>
