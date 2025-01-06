6902c8bad11bff6b2d7c2a90e91877d350c31a62
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-01-05 20:54:25 -0900

    feat(textboxinput): creating dynmic input

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,5 @@
+import SignInForm from "./components/signin-form";
+
 export default async function SignIn() {
-  return (
-    <div className="text-5xl flex justify-center items-center">
-      <h1>this is signin page</h1>
-    </div>
-  );
+  return <SignInForm />;
 }
